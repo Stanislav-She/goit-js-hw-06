e023438f810ae4e3986726a8fdba1a0b7074f1a7
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-07 17:03:10 +0200

    Formatting the first task
    - Formatting the first task
    - Start of work on the second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.createElement("li");
+// ==> створюємо в пам'яті окремий елемент li
+listRef.classList.add("item");
+// ==> на створений елемент додаємо клас item за методом element.classList.add()
+console.log(listRef);
