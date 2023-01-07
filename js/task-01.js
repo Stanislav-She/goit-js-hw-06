cc5907b83fbcb1b8802f5687fe86dcec4df3bbf6
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-07 15:33:37 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const itemRef = document.querySelector("#categories").children;
+// повертає псевдомасив з дітей ul з id="categories"
+// console.log(itemRef);
+console.log("Number of categories:", itemRef.length);
+// виводить довжину масиву, відповідно кількість категорій
