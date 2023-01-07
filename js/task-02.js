22ed7ee52a9498a1ba2b76d652c6783b410b2a69
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-07 18:27:31 +0200

    Completes the task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,8 +7,26 @@ const ingredients = [
   "Condiments",
 ];
 
-const listRef = document.createElement("li");
+const listRef = document.querySelector("#ingredients");
+
+[...ingredients].forEach((element) => {
+  const itemRef = document.createElement("li");
+  itemRef.classList.add("item");
+  itemRef.textContent = element;
+  listRef.appendChild(itemRef);
+});
+
+// const listRef = document.querySelector("#ingredients");
+// ==> отримуємо посилання на ul з html
+
+// [...ingredients].forEach((element) => {
+// const itemRef = document.createElement("li");
 // ==> створюємо в пам'яті окремий елемент li
-listRef.classList.add("item");
+// itemRef.classList.add("item");
 // ==> на створений елемент додаємо клас item за методом element.classList.add()
-console.log(listRef);
+// itemRef.textContent = element;
+// ==> присвоюємо кожній лішці текстовий контект з element, тобто один із інгридієнтів
+// listRef.appendChild(itemRef);
+// ==> додаємо всі лішки в ul
+// });
+// 01:20 Заняття 11
