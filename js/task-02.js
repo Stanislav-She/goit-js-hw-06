8a8552553c01695c8026bcb91ba256798a8a539f
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-10 10:44:13 +0200

    Refactoring the task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,14 +9,39 @@ const ingredients = [
 
 const listRef = document.querySelector("#ingredients");
 
-[...ingredients].forEach((element) => {
-  const itemRef = document.createElement("li");
-  itemRef.classList.add("item");
-  itemRef.textContent = element;
-  {
-    listRef.appendChild(itemRef);
-  }
-});
+const makeItemRef = (ingredient) => {
+  return ingredients.map((ingredient) => {
+    const itemRef = document.createElement("li");
+    itemRef.textContent = ingredient;
+    itemRef.classList.add("item");
+    return itemRef;
+  });
+};
+
+listRef.append(...makeItemRef(ingredients));
+
+// ДРУГИЙ ВАРІАНТ
+// const listRef = document.querySelector("#ingredients");
+// // ==> отримуємо посилання на ul з html
+
+// const makeItemRef = (ingredient) => {
+//   return ingredients.map((ingredient) => {
+//     const itemRef = document.createElement("li");
+//     itemRef.textContent = ingredient;
+//     itemRef.classList.add("item");
+//     // ==> на створений елемент додаємо клас item за методом element.classList.add()
+//     return itemRef;
+//   });
+// };
+// // створюємо функцію makeItemRef для перебору масиву з інгридієнтів та повертає на кожній
+// // ітерації трансформує масив ingredients в новий масив itemRef, тобто кінцевим результатом
+// // функції makeItemRef буде новий масив itemRef, який перед цим створює li елементи, на кожній
+// // ітерації додає в textContent цих лішок значення перебраного ingredient та додає клас item
+
+// listRef.append(...makeItemRef(ingredients));
+// // додаємо до ul розпилений отриманий новий масив даних з лішок і текстового контенту всередині них
+
+// ПЕРШИЙ ВАРІАНТ
 
 // const listRef = document.querySelector("#ingredients");
 // ==> отримуємо посилання на ul з html
